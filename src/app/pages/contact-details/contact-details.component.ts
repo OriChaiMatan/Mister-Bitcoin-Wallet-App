687c83c04dc60e35f306951dfb192a5c478193b2
OriChaiMatan <ori.chaimatan@gmail.com>
2024-08-11 23:17:09 +0300

    Render contact details data

--- src/app/pages/contact-details/contact-details.component.ts
+++ src/app/pages/contact-details/contact-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contact } from '../../models/contact.model';
 
 @Component({
   selector: 'contact-details',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class ContactDetailsComponent {
 
+  contact: Contact = {
+    _id: "5a56640269f443a5d64b32ca",
+    name: "Ochoa Hyde",
+    email: "ochoahyde@renovize.com",
+    phone: "+1 (968) 593-3824"
+  };
 }
