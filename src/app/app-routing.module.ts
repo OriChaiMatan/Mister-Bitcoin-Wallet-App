b07c34e6e306ec803cd69cbc0c5f0a10e6c006ac
OriChaiMatan <ori.chaimatan@gmail.com>
2024-08-12 19:55:35 +0300

    Setup basic Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { StatisticPageComponent } from './pages/statistic-page/statistic-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { ContactDetailsComponent } from './pages/contact-details/contact-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'statistics', component: StatisticPageComponent },
+  { path: 'contact', component: ContactPageComponent },
+  { path: 'contact/:id', component: ContactDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
