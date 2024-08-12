9420c4625435799b0d7c93ffbf561259599ee0d6
OriChaiMatan <ori.chaimatan@gmail.com>
2024-08-12 20:22:03 +0300

    Connected ContactDeatils page to route-params by id

--- src/app/pages/contact-details/contact-details.component.ts
+++ src/app/pages/contact-details/contact-details.component.ts
@@ -1,17 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output, inject  } from '@angular/core';
+import { map, Observable, switchMap } from 'rxjs';
 import { Contact } from '../../models/contact.model';
+import { ContactService } from '../../services/contact.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'contact-details',
   templateUrl: './contact-details.component.html',
   styleUrl: './contact-details.component.scss'
 })
-export class ContactDetailsComponent {
+export class ContactDetailsComponent implements OnInit, OnDestroy {
 
-  contact: Contact = {
-    _id: "5a56640269f443a5d64b32ca",
-    name: "Ochoa Hyde",
-    email: "ochoahyde@renovize.com",
-    phone: "+1 (968) 593-3824"
-  };
+  @Input() contactId!: string
+  @Output() back = new EventEmitter()
+
+  private contactService = inject(ContactService)
+  private route = inject(ActivatedRoute)
+  private router = inject(Router)
+
+  contact$: Observable<Contact> = this.route.data.pipe(map(data => data['contact']))
+
+  ngOnInit() {
+    this.contact$ = this.route.params.pipe(
+            switchMap(params => this.contactService.getContactById(params['id']))
+        )
+}
+
+onBack() {
+  this.router.navigateByUrl('/contact')
+}
+
+ngOnDestroy(): void {
+}
 }
