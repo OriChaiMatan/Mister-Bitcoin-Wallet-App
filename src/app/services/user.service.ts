434a2583ee901f9e7cfafd29b4d3a38c7a3975f1
OriChaiMatan <ori.chaimatan@gmail.com>
2024-08-12 22:51:03 +0300

    Addeed get loggedin user option

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user.model';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
 })
 export class UserService {
 
-    constructor() { }
+    user = {
+        name: "Ori Chai-Matan",
+        coins: 100,
+        moves: []
+    }
 
-    public getUser(): User {
-        return {
-            name: "Ori Chai-Matan",
-            coins: 100,
-            moves: []
-        };
+    // public getUser(): User {
+    //     return {
+    //         name: "Ori Chai-Matan",
+    //         coins: 100,
+    //         moves: []
+    //     };
+    // }
+
+    private _loggedInUser$ = new BehaviorSubject(this.user)
+    public loggedInUser$ = this._loggedInUser$.asObservable()
+
+    getUser() {
+        return this._loggedInUser$.value
     }
 }
