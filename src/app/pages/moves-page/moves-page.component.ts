c39ac5b9de85af580b7ed73b6a88a842a865d54e
OriChaiMatan <ori.chaimatan@gmail.com>
2024-08-13 20:35:36 +0300

    Added logic to Moves-page and render the moves list

--- src/app/pages/moves-page/moves-page.component.ts
+++ src/app/pages/moves-page/moves-page.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { User } from '../../models/user.model';
+import { UserService } from '../../services/user.service';
+import { BitcoinService } from '../../services/bitcoin.service';
+import { Router } from '@angular/router';
+import { Move } from '../../models/move.model';
 
 @Component({
   selector: 'moves-page',
   templateUrl: './moves-page.component.html',
   styleUrl: './moves-page.component.scss'
 })
-export class MovesPageComponent {
+export class MovesPageComponent implements OnInit {
+  user!: User;
+  moves$: Observable<Move[]> = of([]);
 
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    const user = this.userService.getUser(); 
+
+  if (!user) {
+    this.router.navigate(['/signup']);
+    return; 
+  }
+
+  this.user = user as User;
+  this.moves$ = of(this.user.moves.reverse());
+  }
 }
